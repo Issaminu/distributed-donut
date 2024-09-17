e283877647be35de46b4c237e6adfac58f849af1
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2024-09-17 13:39:38 +0100

    Rename Work to Chunk

--- work.go
+++ work.go
@@ -18,35 +18,35 @@ import (
 // These are 13 possible values. 13 in binary is 1101. So, we can represent each character as a 4-bit binary number.
 // This means that the total length of the frame is 1760 * 4 = 7040 bits = 880 bytes.
 const FrameSize = 880 // bytes
-// Length of a Work is FrameSize + 4 + 4 = 888 bytes
-const WorkSize = (FrameSize + 4 + 4) * FramesPerChunk // bytes
+// Length of a Chunk is FrameSize + 4 + 4 = 888 bytes
+const ChunkSize = (FrameSize + 4 + 4) * FramesPerChunk // bytes
 
-type Work struct {
-	startFrame    uint32
-	endFrame      uint32
-	workPerformed []byte
+type Chunk struct {
+	startFrame     uint32
+	endFrame       uint32
+	chunkPerformed []byte
 }
 
-func NewWork(data []byte) (*Work, error) {
-	startFrame, endFrame, workPerformed, err := covertBinaryToWorkFormat(data)
+func NewChunk(data []byte) (*Chunk, error) {
+	startFrame, endFrame, chunkPerformed, err := covertBinaryToChunkFormat(data)
 	if err != nil {
 		return nil, err
 	}
 
-	work := &Work{
-		startFrame:    startFrame,
-		endFrame:      endFrame,
-		workPerformed: workPerformed,
+	chunk := &Chunk{
+		startFrame:     startFrame,
+		endFrame:       endFrame,
+		chunkPerformed: chunkPerformed,
 	}
-	return work, nil
+	return chunk, nil
 }
 
-func covertBinaryToWorkFormat(data []byte) (uint32, uint32, []byte, error) {
-	if len(data) != WorkSize {
-		return 0, 0, []byte{}, errors.New("invalid work size")
+func covertBinaryToChunkFormat(data []byte) (uint32, uint32, []byte, error) {
+	if len(data) != ChunkSize {
+		return 0, 0, []byte{}, errors.New("invalid chunk size")
 	}
 	startFrame := binary.BigEndian.Uint32(data[0:4])
 	endFrame := binary.BigEndian.Uint32(data[4:8])
-	var workPerformed = data[8:]
-	return startFrame, endFrame, workPerformed, nil
+	var chunkPerformed = data[8:]
+	return startFrame, endFrame, chunkPerformed, nil
 }
